0e318baba5d0031a5a36a1c6d3edfc236412c6dc
Sreyes V <sreyesv24@gmail.com>
2025-07-05 23:01:19 +0530

    added scroll animation for solar steps in larger device

--- frontend/src/components/Home/home.tsx
+++ frontend/src/components/Home/home.tsx
@@ -1,9 +1,9 @@
-// src/components/Home/home.tsx
-"use client"; 
+"use client";
+
+import React, { useState, useEffect } from 'react';
 
 import Certified from '@/components/certified-by';
 import HeroHome from '@/components/Home/HeroHome';
-import SolarSteps from '@/components/Home/solar-steps';
 import HomeTestimonial from '@/components/Home/Testimomial';
 import Booking from './Booking';
 import Services from '../Services';
@@ -12,15 +12,40 @@ import Faq from './Faq';
 import Partners from './Partners';
 import SolarAdvantageMain from '../SolarCalculator/SolarAdvantageMain';
 
+import SolarStepsGSAP from './Solar-steps-gsap';
+import SolarStepsNoGSAP from './Solar-steps-nogsap';
+
 export default function Main() {
- 
+  // State to track if the screen width is large enough for GSAP animations
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    // Function to update the screen size state
+    const handleResize = () => {
+      // Set isLargeScreen to true if window width is 1024px or greater
+      setIsLargeScreen(window.innerWidth > 1024);
+    };
+
+    // Set initial state on component mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function: remove event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array ensures this effect runs only once on mount and unmount
+
   return (
     <section className='font-switzer'>
       <HeroHome />
       <Certified />
       <SolarAdvantageMain/>
       <HomeTestimonial />
-      <SolarSteps />
+      {/* Conditionally render SolarSteps component based on screen size */}
+      {isLargeScreen ? <SolarStepsGSAP /> : <SolarStepsNoGSAP />}
       <Booking />
       <Services serviceTitle="We help you all the way" />
       <GroupPurchaseScheme />
