c285fcaeb8001f823bb48661c3af43bd6eece4a9
Sreyes V <sreyesv24@gmail.com>
2025-08-07 21:27:00 +0530

    added sitemap dynamically

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://flarize.com/', // 🔁 Change this to your actual domain
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './public',
+};
