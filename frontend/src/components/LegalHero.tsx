61cb66fd800863c3a56d73bed053eebd299e7f0c
Harii2005 <harikrishnankrkv@gmail.com>
2025-12-29 11:32:37 +0530

    fixed deployment issue

--- frontend/src/components/LegalHero.tsx
+++ frontend/src/components/LegalHero.tsx
@@ -17,7 +17,7 @@ export default function LegalHero({
         
               {/* Content */}
               <div className="relative z-10  mx-auto px-4 pt-32 pb-8 md:pt-48 md:pb-12 max-w-7xl flex flex-col md:flex-row  h-full lg:gap-40 gap-0">
-                {/* Title and description*/
+                {/* Title and description*/}
                 <div className="w-full  text-left ">
                     <h1 className="text-4xl sm:text-5xl lg:text-[64px] font-semibold text-[#123532] mb-4 ">
                         {title}
--- frontend/src/components/Projects/ProjectHero.tsx
+++ frontend/src/components/Projects/ProjectHero.tsx
@@ -15,7 +15,7 @@ export default function ProjectHero({
         
               {/* Content */}
               <div className="relative z-10  mx-auto px-4 pt-32 pb-8 md:pt-48 md:pb-12 max-w-7xl flex flex-col md:flex-row  h-full lg:gap-40 gap-0">
-                {/* Title and description*/
+                {/* Title and description*/}
                 <div className="w-full lg:w-2xl text-center lg:text-left ">
                     <h1 className="text-[40px]/10 sm:text-5xl lg:text-7xl font-medium text-[#123532] mb-4 ">
                         {title}
