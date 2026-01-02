826bc40472251a9b1c2e954a83c0d891e1e6f2ea
Harii2005 <harikrishnankrkv@gmail.com>
2026-01-02 21:36:55 +0530

    updated navbar (blog)

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
             </li>
             <li>
               <Link
-                href="/"
+                href="/" // Temporary: redirects to home instead of /resources 
                 className={`relative lg:text-xl ${
                   isActive("/resources") ? "font-bold before:w-1/2" : ""
                 } hover:font-bold transition-all duration-200 before:content-[''] before:absolute before:bottom-[-0.25rem] before:left-0 before:h-[0.1875rem] before:rounded-lg before:bg-[#FBC207] before:w-0 before:transition-all before:duration-300 before:ease-in-out hover:before:w-1/2`}
@@ -126,7 +126,7 @@ export default function Header() {
         </li>
         <li>
           <Link
-            href="/"
+            href="/" // Temporary: redirects to home instead of /resources 
             onClick={() => setIsOpen(false)}
             className={`relative text-gray-700 font-medium ${
               isActive("/resources") ? "font-bold" : ""
