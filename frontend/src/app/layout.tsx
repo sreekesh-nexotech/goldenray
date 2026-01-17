46454dd19017ebeac1565e88a57d8348ec8519e7
Harii2005 <harikrishnankrkv@gmail.com>
2026-01-17 15:36:39 +0530

    updated GSC and GTM

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -129,9 +129,38 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${dmSans.variable} ${switzer.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5H47L3GM');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* Google Search Console Verification */}
+        <meta
+          name="google-site-verification"
+          content="yPvkA7y4qK17chMlAbn958D0Nfhe7AWGEAIzwGJ4Cec"
+        />
+
         <meta name="apple-mobile-web-app-title" content="Flarize" />
       </head>
       <body className="font-switzer">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5H47L3GM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Header />
         {children}
         <Footer />
