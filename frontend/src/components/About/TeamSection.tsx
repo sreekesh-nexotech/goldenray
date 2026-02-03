7eb9cd27bee5cdc9371edbfc05a4d7977ef1ad43
Harii2005 <harikrishnankrkv@gmail.com>
2026-02-03 23:32:46 +0530

    updated images in Team Member

--- frontend/src/components/About/TeamSection.tsx
+++ frontend/src/components/About/TeamSection.tsx
@@ -9,7 +9,7 @@ const teamMembers = [
     // description:
     //   "From installations to customer support, Mark makes sure everything runs smoothly. His mission? A seamless, hassle-free solar experience for every customer.",
     linkedin: "https://www.linkedin.com/in/jude-james-38b45263/",
-    imageUrl: "https://golden-ray.b-cdn.net/images/Jude%20James%20(1).jpeg",
+    imageUrl: "https://golden-ray.b-cdn.net/About%20us/Team/Jude%20James.jpeg",
   },
   {
     name: "AROMAL KM",
@@ -18,7 +18,7 @@ const teamMembers = [
     //   "Sarah is the mastermind behind Flarece's cutting-edge solar tech. With years of experience in renewable energy, she ensures our solutions are efficient, and future-proof.",
     linkedin:
       "https://www.linkedin.com/in/aromal-km-627ba3325/?originalSubdomain=in",
-    imageUrl: "https://golden-ray.b-cdn.net/images/Aromal%20KM%20(1).jpg",
+    imageUrl: "https://golden-ray.b-cdn.net/About%20us/Team/Aromal%20KM.jpg",
   },
   {
     name: "ANANTHAKRISHNAN G",
@@ -28,7 +28,7 @@ const teamMembers = [
     linkedin:
       "https://www.linkedin.com/in/ananthakrishnan-g-4421b1276?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     imageUrl:
-      "https://golden-ray.b-cdn.net/images/Ananthakrishnnan%20G%20(1).jpeg",
+      "https://golden-ray.b-cdn.net/About%20us/Team/Ananthakrishnnan%20G.jpeg",
   },
   {
     name: " ANUPRIYA MS ",
