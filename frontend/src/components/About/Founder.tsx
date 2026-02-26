d206df246feefab7e41174685f3e2c045e4857e2
Harii2005 <harikrishnankrkv@gmail.com>
2026-02-26 19:56:11 +0530

    updated youTube video in the about page

--- frontend/src/components/About/Founder.tsx
+++ frontend/src/components/About/Founder.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export default function Founder(){
-
-    const youtubeLink = "https://www.youtube.com/embed/jKLCiCoFngA";//link for the video
+export default function Founder() {
+  const youtubeLink =
+    "https://www.youtube.com/embed/uzDu-Vn7a0Q?autoplay=0&mute=1&rel=0"; //link for the video
   return (
     <div className="flex flex-col gap-5 justify-between lg:flex-row bg-[#074A4D] text-[#DBD8D8] rounded-3xl p-8 lg:p-12 max-w-full mx-auto mb-18">
-        {/* Right Section: YouTube Video */}
+      {/* Right Section: YouTube Video */}
       <div className="block lg:hidden w-full mb-8 md:mt-0">
         <div className="relative w-full h-0 pb-[56.25%] rounded-lg overflow-hidden">
           <iframe
@@ -13,7 +13,8 @@ export default function Founder(){
             src={youtubeLink}
             title="YouTube video"
             frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            loading="lazy"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         </div>
@@ -21,11 +22,16 @@ export default function Founder(){
       {/* Left Section: Quote and Name */}
       <div className="lg:w-[34%] flex flex-col justify-between">
         <p className="text-base sm:text-xl font-normal mb-4 lg:mb-0">
-          <q>At Flarize, our team is made up of passionate solar professionals, engineers, and problem-solvers who care deeply about the
-homes and businesses we serve across Kerala. We understand the trust people place in us when they choose solar, and we take
-that responsibility seriously. Guided by honesty, quality workmanship, and long-term reliability, we work together to deliver
-solar solutions that bring real savings, peace of mind, and a cleaner future for generations to come.
-</q>
+          <q>
+            At Flarize, our team is made up of passionate solar professionals,
+            engineers, and problem-solvers who care deeply about the homes and
+            businesses we serve across Kerala. We understand the trust people
+            place in us when they choose solar, and we take that responsibility
+            seriously. Guided by honesty, quality workmanship, and long-term
+            reliability, we work together to deliver solar solutions that bring
+            real savings, peace of mind, and a cleaner future for generations to
+            come.
+          </q>
         </p>
         <div>
           <h2 className="text-[32px] sm:text-[40px] font-bold">OUR TEAM</h2>
@@ -40,12 +46,12 @@ solar solutions that bring real savings, peace of mind, and a cleaner future for
             src={youtubeLink}
             title="YouTube video"
             frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            loading="lazy"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         </div>
       </div>
     </div>
   );
-};
-
+}
--- frontend/src/components/About/VideoSection.tsx
+++ frontend/src/components/About/VideoSection.tsx
@@ -1,5 +1,6 @@
 export default function VideoSection() {
-  const youtubeLink = "https://www.youtube.com/embed/tKm2-7w0U6A";
+  const youtubeLink =
+    "https://www.youtube.com/embed/uzDu-Vn7a0Q?autoplay=0&mute=1&rel=0";
   return (
     <section className="relative mx-auto px-4 sm:px-6 lg:px-8 xl:px-36 lg:py-24">
       <div className="relative w-full h-0 pb-[56.25%] rounded-lg overflow-hidden">
@@ -8,7 +9,8 @@ export default function VideoSection() {
           src={youtubeLink}
           title="Team Collaboration Video"
           frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          loading="lazy"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
       </div>
