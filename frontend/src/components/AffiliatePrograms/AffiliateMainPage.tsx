967a839810e2275c341ecde4ee7e804633416582
sre-yes-v <sreyesv24@gmail.com>
2026-05-13 01:27:03 +0530

    added earnings component in affialeted page

--- frontend/src/components/AffiliatePrograms/AffiliateMainPage.tsx
+++ frontend/src/components/AffiliatePrograms/AffiliateMainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AffliateHero from './AffliateHero'
 import { WhoCanJoin } from './WhoCanJoin'
+import Earnings from './Earnings'
 
 const AffiliateMainPage = () => {
   return (
     <section className="relative">
       <AffliateHero/>
       <WhoCanJoin/>
+      <Earnings/>
     </section>
   )
 }
